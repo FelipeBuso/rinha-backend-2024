ef0ae92686e64569973dcccb44315e4d5d165dfb
felipebuso <felipe.buso@hotmail.com>
2024-02-29 10:38:28 -0300

    feat-schemas

--- app/schemas.py
+++ app/schemas.py
@@ -1,24 +1,25 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
-from database import Base
+
+from .database import Base
 
 
 class Client(Base):
     __tablename__ = "clientes"
-    id = Column(Integer, primary_key=True)
-    nome = Column(String)
-    limite = Column(Integer)
-    saldo = Column(Integer, nullable=True)
 
-    transacoes = relationship("Transaction", back_populates="cliente")
+    id = Column(Integer, primary_key=True)
+    limite = Column(Integer, default=0)
+    saldo = Column(Integer, default=0)
+
+    f_transacoes = relationship("Transaction", back_populates="cliente")
 
 
 class Transaction(Base):
     __tablename__ = "transacoes"
+
     id = Column(Integer, primary_key=True)
     valor = Column(Integer, nullable=False)
     tipo = Column(String, nullable=False)
-    descricao = Column(String, nullable=False)
     client_id = Column(Integer, ForeignKey("clientes.id"))
 
-    cliente = relationship("Client", back_populates="transacoes")
+    cliente = relationship("Client", back_populates="f_transacoes")
